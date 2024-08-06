e25314eed98e9ec59f4e2b8d82312e0f2c7ec0d2
Attiqa Z <attiqazaki1@gmail.com>
2024-08-05 22:25:32 -0700

    Calculator
    I Create a Calculator using Html, Css and JS.

---
+++ Calculator/script.js
@@ -0,0 +1,16 @@
+let result = document.getElementById("result");
+
+function getResult(value) {
+  result.value += value;
+}
+
+function clearResult() {
+  result.value = "";
+}
+function backspace() {
+  result.value = result.value.slice(0, -1);
+}
+
+function calculateResult() {
+  result.value = eval(result.value);
+}
